81d8095cb2767c96b2710274ada4bb2e1bfc60f0
LeoLikeLink <LeoLauenstein@gmx.de>
2024-06-24 17:51:17 +0200

    Added depth and time as variable

--- testLeo.py
+++ testLeo.py
@@ -4,13 +4,15 @@ import chess.engine
 engine_path = "fairy-stockfish_x86-64-bmi2.exe"
 
 
-def evaluate_board(board: chess.Board):
+def evaluate_board(board, depth, time):
     with chess.engine.SimpleEngine.popen_uci(engine_path) as engine:
-        result = engine.analyse(board, chess.engine.Limit(time=0.1))
+        result = engine.analyse(board, chess.engine.Limit(time=time,depth=depth))
         return result['score'].relative
     
 board = chess.Board() 
-print(evaluate_board(board))
+time = 20
+depth = 20
+print(evaluate_board(board,depth,time))
 
 
 
